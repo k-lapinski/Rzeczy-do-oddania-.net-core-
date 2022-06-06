35a89fe57d09bc47294245afa450629aba9022ac
k-lapinski <k.lapinski97@gmail.com>
2022-06-06 19:29:03 +0200

    signalr

---
+++ rzeczy_do_oddaniaNEW/Pages/Hubs/ChatHub .cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace rzeczy_do_oddaniaNEW.Pages.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
