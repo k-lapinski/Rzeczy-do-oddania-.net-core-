bb69c7b3903ebf2d05fff02bbf28360346d93f69
Damian Maksimowicz <qCob321@gmail.com>
2022-06-06 20:09:16 +0200

    added button reservation

--- rzeczy_do_oddaniaNEW/Pages/Models/Item.cs
+++ rzeczy_do_oddaniaNEW/Pages/Models/Item.cs
@@ -6,31 +6,32 @@ namespace rzeczy_do_oddaniaNEW.Pages.Models
     public class Item
     {   [Key]
         public int ID { get; set; }
-        [Display(Name = "Tytuł")]
+        [Display(Name = "Title")]
         [Required]
         [MaxLength(100)]
         public string Title { get; set; } = string.Empty;
-        [Display(Name = "Opis")]
+        [Display(Name = "Description")]
         [Required]
         [MaxLength(300)]
         public string Description { get; set; } = string.Empty;
 
-        [Display(Name = "Adres")]
+        [Display(Name = "Address")]
         [Required]
         [MaxLength(300)]
         public string Address { get; set; } = string.Empty;
 
-        [Display(Name = "Data zakończenia")]
+        [Display(Name = "End day")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        [Display(Name = "Kategoria")]
+        [Display(Name = "Category")]
         public string Category { get; set; } = string.Empty;
-        [Display(Name = "Zdjęcie")]
+        [Display(Name = "Photo")]
         [MaxLength(300)]
         public string Image { get; set; } = string.Empty;
 
         public string userID { get; set; } = string.Empty;
 
+        public string Reservation { get; set; } = string.Empty; 
 
     }
 }
