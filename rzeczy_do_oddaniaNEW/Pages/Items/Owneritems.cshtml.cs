07a7145d1fb2a986bac0840662065142fd00de85
k-lapinski <k.lapinski97@gmail.com>
2022-06-08 11:10:08 +0200

    added OwnerItem website

---
+++ rzeczy_do_oddaniaNEW/Pages/Items/Owneritems.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using rzeczy_do_oddaniaNEW.Data;
+using rzeczy_do_oddaniaNEW.Pages.Models;
+
+namespace rzeczy_do_oddaniaNEW.Pages.Owner
+{
+    public class IndexModel : PageModel
+    {
+        private readonly rzeczy_do_oddaniaNEW.Data.ApplicationDbContext _context;
+
+        public IndexModel(rzeczy_do_oddaniaNEW.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Item> Item { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Item != null)
+            {
+                
+                Item = await _context.Item.ToListAsync();
+            }
+        }
+    }
+}
